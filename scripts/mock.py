89c0cfdf03497841e8fe7e1b1be46917adec9809
mingminyu <yu_mingm623@163.com>
2024-01-19 00:02:48 +0800

    updated

--- scripts/mock.py
+++ scripts/mock.py
@@ -1,5 +1,6 @@
-from tortoise import Tortoise, run_async
+# encoding: utf-8
 from typing import Dict, Any
+from tortoise import Tortoise, run_async
 from atomui.models.chat import ChatInfo, ChatMessageInfo
 
 
@@ -67,7 +68,7 @@ chat_messages = [
         "content": "Hello, Atom",
         "func_call": None,
         "func_call_params": None,
-        "create_time": "2024-01-15 14:25:00"
+        "create_time": "2024-01-15 14:25:01"
     },
     {
         "cid": "164334f0-bc9f-4e4a-9b12-038a201f7387",
@@ -76,7 +77,43 @@ chat_messages = [
         "content": "I'm AtomGPT, Can I help you?",
         "func_call": None,
         "func_call_params": None,
-        "create_time": "2024-01-15 14:25:00"
+        "create_time": "2024-01-15 14:25:02"
+    },
+    {
+        "cid": "b4cba9bc-e26b-4d28-b206-4c26aee5e64f",
+        "index": 1,
+        "role": "user",
+        "content": "在ECharts关系图中实现自动布局",
+        "func_call": None,
+        "func_call_params": None,
+        "create_time": "2024-01-15 14:25:01"
+    },
+    {
+        "cid": "b4cba9bc-e26b-4d28-b206-4c26aee5e64f",
+        "index": 2,
+        "role": "assistant",
+        "content": "在 ECharts 中，如果你希望自动排列节点坐标，你可以使用布局算法。ECharts 提供了一些布局算法，其中之一是力导向图（Force-directed Graph）布局，它可以自动调整节点的位置，使得图形更加美观。\n\n以下是一个简单的力导向图布局的示例：\n\n```javascript\noption = {\n    series: [{\n        type: 'graph',\n        layout: 'force',\n        force: {\n            repulsion: 100,  // 节点之间的斥力\n            edgeLength: 150,  // 连接线的默认长度\n        },\n        data: [{\n            name: 'Node 1',\n        }, {\n            name: 'Node 2',\n        }, {\n            name: 'Node 3',\n        }],\n        links: [{\n            source: 'Node 1',\n            target: 'Node 2',\n        }, {\n            source: 'Node 2',\n            target: 'Node 3',\n        }],\n    }],\n};\n```\n\n在这个例子中，通过将 `layout` 设置为 `'force'`，并调整 `repulsion` 参数来调整节点之间的斥力，可以实现自动排列节点坐标。布局算法会根据节点之间的关系和斥力，自动计算节点的位置。\n\n你可以根据实际需求调整 `repulsion` 和其他参数，以便获得满足你设计需求的图形布局。此外，ECharts 还提供其他布局算法，如环形布局、树状布局等，你可以根据具体情况选择合适的布局算法。",
+        "func_call": None,
+        "func_call_params": None,
+        "create_time": "2024-01-15 14:25:01"
+    },
+    {
+        "cid": "ab132efb-fac4-46d9-82ec-ae4c283f180b",
+        "index": 1,
+        "role": "user",
+        "content": "Plotting a graph for atom documents.",
+        "func_call": None,
+        "func_call_params": None,
+        "create_time": "2024-01-15 14:25:01"
+    },
+    {
+        "cid": "6b101588-755f-48ec-bd40-25e1fa3abf1c",
+        "index": 1,
+        "role": "user",
+        "content": "Hello Atom, what day is it today?",
+        "func_call": None,
+        "func_call_params": None,
+        "create_time": "2024-01-15 14:25:01"
     },
 ]
 
@@ -87,10 +124,17 @@ class ChatDBTool:
     async def add_chat_info(chat_info_: Dict[str, Any]):
         await ChatInfo.create(**chat_info_)
 
+    @staticmethod
+    async def add_chat_message(chat_message_: Dict[str, Any]):
+        await ChatMessageInfo.create(**chat_message_)
+
 
 
 if __name__ == "__main__":
     run_async(init())
 
-    for chat_info in chat_infos:
-        run_async(ChatDBTool.add_chat_info(chat_info))
+    # for chat_info in chat_infos:
+    #     run_async(ChatDBTool.add_chat_info(chat_info))
+
+    for chat_message in chat_messages:
+        run_async(ChatDBTool.add_chat_message(chat_message))
--- webui.py
+++ webui.py
@@ -1,30 +1,27 @@
 import asyncio
 from nicegui import ui, app, run
-# from nicegui.elements.chat_message import ChatMessage
 from tortoise import Tortoise
+from typing import List
 
-# from nicegui.events import ValueChangeEventArguments
-# from atomui import to_ref, effect
 from atomui import webui
 from atomui.components import Router
-from atomui.layout.header import chat_header
-from atomui.layout.sidebar import chat_sidebar_card
-from atomui.layout.footer import chat_footer
 from atomui.elements.chatgpt import chat_greet
 
-from atomui.mock.chat_conversation import chat_conversations_example
-from atomui.models.chat import ChatCardModel, ChatInfo
-from atomui.elements.drawer import DrawerBindableUi
 from atomui.utils.parser import MarkdownParser
-from atomui.layout.body import chat_messages_card
-from atomui.models.chat import ChatMessageModel
-from atomui.elements.chatgpt import ChatFooter, ChatSidebar, ChatHeader
-from orm.chat import ChatInfo
+from atomui.elements.chatgpt import ChatFooter, ChatSidebar, ChatHeader, ChatMessageCard1, ChatSampleCard
+from orm.chat import ChatInfo, ChatMessageInfo
+
+# from atomui.layout.header import chat_header
+# from atomui.layout.footer import chat_footer
+# from atomui.layout.body import chat_messages_card
+# from atomui.layout.sidebar import chat_sidebar_card
+# from atomui.elements.drawer import DrawerBindableUi
+# from atomui.mock.chat_conversation import chat_conversations_example
+# from atomui.models.chat import ChatMessageModel
+# from atomui.models.chat import ChatCardModel, ChatInfo
 
 md_parser = MarkdownParser()
 app.add_static_files('/static', 'static')
-
-# chat_cards = [ChatCardModel(**chat_conversation) for chat_conversation in chat_conversations_example]
 
 
 async def init_db() -> None:
@@ -40,16 +37,38 @@ app.on_startup(init_db)
 app.on_shutdown(close_db)
 
 
-
-# def get_chat_card(chat_id: str):
-#     for chat_card in chat_cards:
-#         if chat_card.cid == chat_id:
-#             return chat_card
-
+async def get_chat_infos(username: str) -> List[ChatInfo]:
+    from datetime import datetime, timedelta
+    thirty_days_ago = datetime.now() - timedelta(days=30)
+    return await ChatInfo.filter(username=username, status=True, ts__gte=thirty_days_ago).order_by('ts')
 
 
-async def get_chat_infos(username: str):
-    return await ChatInfo.filter(username=username).order_by('ts')
+async def get_chat_messages(chat_id: str) -> List[ChatMessageInfo]:
+    return await ChatMessageInfo.filter(cid=chat_id).order_by('index')
+
+
+chat_sample_cards = [
+    {
+        "title": "Make a content strategy",
+        "description": "for a newsletter featuring free weekend events",
+        "user_input": "Make a content strategy for a newsletter featuring free weekend events"
+    },
+    {
+        "title": "Create a personal webpage",
+        "description": "for a newsletter featuring free weekend events",
+        "user_input": "Create a personal webpage for me, all in a single file. Ask me 3 questions first on whatever you need to know."
+    },
+    {
+        "title": "Plan a trip",
+        "description": "to see the best of New York in 3 days",
+        "user_input": "I'll be in New York for 3 days. Can you recommend what I should do to see the best of the city?"
+    },
+    {
+        "title": "Show me a code snippet",
+        "description": "of a website's sticky header",
+        "user_input": "Show me a code snippet of a website's sticky header in CSS and JavaScript."
+    },
+]
 
 
 @ui.page("/")
@@ -59,37 +78,29 @@ async def main():
 
     @router.add('/')
     def index():
+        # ui.label("下面试试 tw css 的")
+        # ui.label("随屏幕大小，颜色变化").classes("max-[2800px]:pt-[60px] min-[1800px]:pt-[360px]")
+
         chat_greet()
-        with ui.row().classes("w-full flex justify-center self-center items-center pt-[260px] portrait:pt-[1350px]"):
+        sample_cards_grid_css = (
+            "w-full flex justify-center self-center items-center portrait:pt-[1350px] "
+            "max-[2800px]:pt-[260px] min-[1800px]:pt-[360px]"
+        )
+        with ui.row().classes(sample_cards_grid_css):
             with ui.grid(rows=2, columns=2).classes("max-w-6xl self-center items-center"):
-                with webui.card(bordered=True).tight().classes('max-w-3xl justify-center self-center items-center'):
-                    with webui.card_section().classes('w-3xl'):
-                        webui.label("Make a content strategy").classes('text-bold text-blue-10')
-                        webui.label("for a newsletter featuring free weekend events").classes('text-grey-6')
-
-                with webui.card(bordered=True).tight().classes('max-w-3xl self-center items-center'):
-                    with webui.card_section().classes('w-3xl'):
-                        webui.label("Make a content strategy").classes('text-bold text-blue-10')
-                        webui.label("for a newsletter featuring free weekend events").classes('text-grey-6')
-
-                with webui.card(bordered=True).tight().classes('max-w-3xl flex justify-center self-center items-center'):
-                    with webui.card_section().classes('w-3xl'):
-                        webui.label("Make a content strategy").classes('text-bold text-blue-10')
-                        webui.label("for a newsletter featuring free weekend events").classes('text-grey-6')
-
-                with webui.card(bordered=True).tight().classes('max-w-3xl flex justify-center self-center items-center'):
-                    with webui.card_section().classes('w-3xl'):
-                        webui.label("Make a content strategy").classes('text-bold text-blue-10')
-                        webui.label("for a newsletter featuring free weekend events").classes('text-grey-6')
-
-        # webui.button('Add new Chat', on_click=lambda: add_new_card(router, chat_sidebar))
-        # chat_messages_card(ChatCard(**chat_conversations_example[1]).conversation)
+                for sample_card_info in chat_sample_cards:
+                    ChatSampleCard(
+                        title=sample_card_info['title'],
+                        description=sample_card_info['description'],
+                        user_input=sample_card_info['user_input']
+                    )
 
     @router.add('/chat')
     async def chat_home(chat_id: str = None):
-        # chat_message = get_chat_card(chat_id)
-        # chat_messages_card(chat_message.conversation)
-        ...
+        chat_messages = await get_chat_messages(chat_id)
+        for message in chat_messages:
+            ChatMessageCard1(message)
+
 
     @router.add('/echarts_graph')
     def example_npm_graph():
@@ -142,18 +153,11 @@ async def main():
 
     app.storage.user.update({"username": "draven"})
     username = app.storage.user.get("username")
-
-
     chat_infos = await get_chat_infos(username)
-    # for chat_info in await get_chat_infos(username):
-    #     print(chat_info)
 
-
-    # left_drawer = chat_sidebar(router)
     chat_sidebar = ChatSidebar(router=router, chat_infos=chat_infos)
     chat_header = ChatHeader(chat_sidebar)
-    # chat_box, send_disabled = chat_footer()
-    # ChatFooter(router=router, chat_sidebar=chat_sidebar)
+    ChatFooter(router=router, chat_sidebar=chat_sidebar)
 
     router.frame().classes('w-full')
 
